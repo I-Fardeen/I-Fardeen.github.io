2782a0660f38bbbfa15b7d276560d606a70ef5a4
Fardeen_Khan <fardeenahmadkhan786@gmail.com>
2026-09-12 22:20:53 +0530

    feat: create a personal portfolio website

---
+++ script.js
@@ -0,0 +1,20 @@
+const themeToggle = document.getElementById("theme-toggle");
+
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme) {
+    document.documentElement.dataset.theme = savedTheme;
+}
+
+themeToggle.addEventListener("click", () => {
+    const currentTheme = document.documentElement.dataset.theme;
+
+    const newTheme =
+        currentTheme === "dark"
+            ? "light"
+            : "dark";
+
+    document.documentElement.dataset.theme = newTheme;
+
+    localStorage.setItem("theme", newTheme);
+});
